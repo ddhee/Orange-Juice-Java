7d38268210f751f0bc23294b9c4db67fa2185e7e
zccz14 <zccz14@outlook.com>
2017-02-24 16:22:20 +0800

    Ready to Deploy

---
+++ src/main/java/ABean.java
@@ -0,0 +1,15 @@
+/**
+ * Class ABean
+ * Created by zccz14 on 2017/2/24.
+ */
+public class ABean {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
